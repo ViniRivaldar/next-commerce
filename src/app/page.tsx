9fa97d5a8bf2a81a866a52c468a976d3e3861077
ViniRivaldar <vinicius-rivaldar@outlook.com>
2024-04-16 15:25:01 -0300

    primeira listagem de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Next E-commerce 14</h1>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 xl:gap-6">
+       
+      </div>
     </div>
   );
 }
