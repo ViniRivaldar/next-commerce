30ce0d5a6b5657b5a69c9503a51ed10eb625709d
ViniRivaldar <vinicius-rivaldar@outlook.com>
2024-04-19 04:46:09 -0300

    corrigindo alguns bugs

--- src/app/components/CartDrawer.tsx
+++ src/app/components/CartDrawer.tsx
@@ -3,6 +3,7 @@ import Image from "next/image"
 import { useCartStore } from "../../store"
 import { formatPrice } from "@/lib/Utils"
 import CheckoutButton from "./CheckoutButton"
+import Checkout from "./Checkout"
 
 export default function CartDrawer(){
     const useStore = useCartStore()
@@ -19,38 +20,43 @@ export default function CartDrawer(){
 
                 <button onClick={()=> useStore.toggleCart()} className="font-bold text-sm 
                 text-teal-600">Voltar para a Loja</button>
-                <div className="border-t border-gray-400 my-4"></div>
-                    {
-                        useStore.cart.map((items)=>(
-                            <div key={items.id} className="flex gap-4 py-4">
-                                <Image 
-                                src={items.image}
-                                alt={items.name}
-                                width={120}
-                                height={120}
-                                className="object-cover"/>
 
-                                <div>
-                                    <h2 className="w-42 truncate">{items.name}</h2>
-                                    <h2>Quantidade: {items.quantity}</h2>
-                                    <p className="text-teal-600 text-sm font-bold">
-                                        {formatPrice(items.price)}
-                                    </p>
-                                    <button className="py-1 px-2 border rounded-md mt-2 text-sm mr-1" 
-                                    onClick={()=> useStore.addProduct(items)}>Adicionar</button>
-                                    <button onClick={()=>useStore.removeProduct(items)}
-                                    className="py-1 px-2 border rounded-md mt-2 text-sm">Remover</button>
-                                </div>
+                <div className="border-t border-gray-400 my-4"></div>
+
+                {useStore.onCheckout === 'cart' &&(
+                <>
+                {useStore.cart.map((items)=>(
+                        <div key={items.id} className="flex gap-4 py-4">
+                            <Image 
+                            src={items.image}
+                            alt={items.name}
+                            width={120}
+                            height={120}
+                            className="object-cover"/>
+
+                            <div>
+                                <h2 className="w-42 truncate">{items.name}</h2>
+                                <h2>Quantidade: {items.quantity}</h2>
+                                <p className="text-teal-600 text-sm font-bold">
+                                    {formatPrice(items.price)}
+                                </p>
+                                <button className="py-1 px-2 border rounded-md mt-2 text-sm mr-1" 
+                                onClick={()=> useStore.addProduct(items)}>Adicionar</button>
+                                <button onClick={()=>useStore.removeProduct(items)}
+                                className="py-1 px-2 border rounded-md mt-2 text-sm">Remover</button>
+                            </div>
 
                                 
-                            </div>
-                        ))
-                    }
-                    {
-                        useStore.cart.length >0 && useStore.onCheckout === 'cart' &&(
-                            <CheckoutButton totalPrice={totalPrice}/>
-                        )
-                    }
+                        </div>
+                    ))}
+                </>
+                 )}   
+                    {useStore.cart.length >0 && useStore.onCheckout === 'cart' &&(
+                        <CheckoutButton totalPrice={totalPrice}/>
+                    )}
+                    {useStore.onCheckout === 'checkout' &&(
+                        <Checkout/>
+                    )}
             </div>
 
         </div>
